3f2934663d5084737a6dfa8be2cf8765f4b1f642
abearab <abolfazl.arab@ucsf.edu>
2024-12-03 15:02:20 -0800

    draft performer methods

--- ezformer/models.py
+++ ezformer/models.py
@@ -1,12 +1,30 @@
+import torch
 import tensorflow as tf
 import tensorflow_hub as tf_hub
+
+import sys
+sys.path.append("enformer_fine_tuning/code")
+from pl_models import LitModelHeadAdapterWrapper
 
 
 #Performer model code
 class Performer:
     #TODO: Implement a class for Perform model, by @shirondru
     #https://github.com/shirondru/enformer_fine_tuning
-    pass
+    def __init__(self, checkpoint_path):
+        self._model = LitModelHeadAdapterWrapper.load_from_checkpoint(checkpoint_path)
+        self._model.eval()
+
+    def predict_on_batch(self, inputs, single_track=True):
+        # Set the model to evaluation mode
+        predictions = self._model(inputs)
+
+        if single_track:
+            # return the predictions (a single number probably from 'Whole Blood' / 'CNhs11761' track)
+            return predictions.cpu().detach().numpy()
+        else:
+            raise NotImplementedError("Currently only single_track=True is supported")
+
 
 #Enformer model code
 class Enformer:
@@ -19,16 +37,16 @@ class Enformer:
         # return a dictionary of the predictions
         return {k: v.numpy() for k, v in predictions.items()}
 
-    def contribution_input_grad(self, input_sequence, target_mask, output_head='human'):
-        input_sequence = input_sequence[tf.newaxis]
-
-        target_mask_mass = tf.reduce_sum(target_mask)
-        with tf.GradientTape() as tape:
-            tape.watch(input_sequence)
-            prediction = tf.reduce_sum(
-                target_mask[tf.newaxis] *
-                self._model.predict_on_batch(input_sequence)[output_head]) / target_mask_mass
-
-        input_grad = tape.gradient(prediction, input_sequence) * input_sequence
-        input_grad = tf.squeeze(input_grad, axis=0)
-        return tf.reduce_sum(input_grad, axis=-1)
+    # def contribution_input_grad(self, input_sequence, target_mask, output_head='human'):
+    #     input_sequence = input_sequence[tf.newaxis]
+    #     
+    #     target_mask_mass = tf.reduce_sum(target_mask)
+    #     with tf.GradientTape() as tape:
+    #         tape.watch(input_sequence)
+    #         prediction = tf.reduce_sum(
+    #             target_mask[tf.newaxis] *
+    #             self._model.predict_on_batch(input_sequence)[output_head]) / target_mask_mass
+    #     
+    #     input_grad = tape.gradient(prediction, input_sequence) * input_sequence
+    #     input_grad = tf.squeeze(input_grad, axis=0)
+    #     return tf.reduce_sum(input_grad, axis=-1)
