44ba8da36c9da7ca61a743c91f4d7cfa094a0ad9
abearab <abolfazl.arab@ucsf.edu>
2024-12-03 20:03:21 -0800

    rename prediction methods for consistency

--- ezformer/models.py
+++ ezformer/models.py
@@ -33,7 +33,7 @@ class Enformer:
     def __init__(self, tfhub_url):
         self._model = tf_hub.load(tfhub_url).model
 
-    def predict_on_batch(self, inputs):
+    def predict(self, inputs):
         predictions = self._model.predict_on_batch(inputs)
         # return a dictionary of the predictions
         return {k: v.numpy() for k, v in predictions.items()}
--- ezformer/predict.py
+++ ezformer/predict.py
@@ -27,7 +27,7 @@ def predict_tracks(models, sequence_one_hot, n_folds=1):
     predicted_tracks = []
 
     for fold_ix in range(n_folds) :
-        yh = models[fold_ix].predict_on_batch(sequence_one_hot[None, ...])['human'][:, None, ...].astype('float16')
+        yh = models[fold_ix].predict(sequence_one_hot[None, ...])['human'][:, None, ...].astype('float16')
         predicted_tracks.append(yh)
 
     predicted_tracks = np.concatenate(predicted_tracks, axis=1)
@@ -48,7 +48,7 @@ def predict_single_track_scalar(models, sequence_one_hot, n_folds=1):
     predictions = []
 
     for fold_ix in range(n_folds) :
-        yh = models[fold_ix].predict_on_batch(sequence_one_hot).astype('float16')
+        yh = models[fold_ix].predict(sequence_one_hot).astype('float16')
         yh = yh[:,yh.shape[1]//2,:] #keep value at center of sequence. The sequence axis is removed
         predictions.append(yh)
 
